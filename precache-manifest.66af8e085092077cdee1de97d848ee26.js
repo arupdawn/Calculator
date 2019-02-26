32eb07da54a611bbf270198893ffa05ef7a0352a
ArupDawn <arupchandradawn@gmail.com>
2019-02-26 09:19:30 +0530

    Updates

---
+++ precache-manifest.66af8e085092077cdee1de97d848ee26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Calculator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "546deb05c16eeec89d41",
+    "url": "/Calculator/static/js/runtime~main.546deb05.js"
+  },
+  {
+    "revision": "ff9e368d67ae5a243fc5",
+    "url": "/Calculator/static/js/main.ff9e368d.chunk.js"
+  },
+  {
+    "revision": "1f2f07d25e04408300a9",
+    "url": "/Calculator/static/js/1.1f2f07d2.chunk.js"
+  },
+  {
+    "revision": "ff9e368d67ae5a243fc5",
+    "url": "/Calculator/static/css/main.3e173ec8.chunk.css"
+  },
+  {
+    "revision": "4a7975fb3850640e6dd3aa5ccdcb5818",
+    "url": "/Calculator/index.html"
+  }
+];
